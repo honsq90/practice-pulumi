55f831ad16c343eb5d6ebf4a0bb1a1837614cf8b
Shuqian Hon (SQ) <sq@waddle.com.au>
2021-05-08 08:46:36 +1000

    Attach custom log group

--- fargate/index.ts
+++ fargate/index.ts
@@ -1,13 +1,17 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as awsx from "@pulumi/awsx";
+import * as aws from "@pulumi/aws";
 
 // Create a load balancer to listen for requests and route them to the container.
 const listener = new awsx.elasticloadbalancingv2.NetworkListener("nginx", { port: 80 });
+
+const logGroup = new aws.cloudwatch.LogGroup("nginx-log")
 
 // Define the service, building and publishing our "./app/Dockerfile", and using the load balancer.
 const service = new awsx.ecs.FargateService("nginx", {
     desiredCount: 2,
     taskDefinitionArgs: {
+        logGroup: logGroup,
         containers: {
             nginx: {
                 image: awsx.ecs.Image.fromPath("nginx", "./app"),
@@ -19,4 +23,6 @@ const service = new awsx.ecs.FargateService("nginx", {
 });
 
 // Export the URL so we can easily access it.
-export const frontendURL = pulumi.interpolate `http://${listener.endpoint.hostname}/`;
+export const frontendURL = pulumi.interpolate `https://${listener.endpoint.hostname}/`;
+
+export const serviceArn = service.urn
