9348f529247b3cee91853975dee0b8a9be7523fd
Shuqian Hon (SQ) <sq@waddle.com.au>
2021-05-09 09:20:34 +1000

    Inline policy statements and create lambda permission

--- fargate/index.ts
+++ fargate/index.ts
@@ -1,6 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as awsx from "@pulumi/awsx";
-import { elasticStream, logGroup } from "./logging/elasticsearch";
+import { cloudwatchLogGroup, elasticStream, logStreamLambda } from "./logging/elasticsearch";
 
 // Create a load balancer to listen for requests and route them to the container.
 const listener = new awsx.elasticloadbalancingv2.NetworkListener("nginx", { port: 80 });
@@ -9,7 +9,7 @@ const listener = new awsx.elasticloadbalancingv2.NetworkListener("nginx", { port
 const service = new awsx.ecs.FargateService("nginx", {
     desiredCount: 2,
     taskDefinitionArgs: {
-        logGroup: logGroup,
+        logGroup: cloudwatchLogGroup,
         containers: {
             nginx: {
                 image: awsx.ecs.Image.fromPath("nginx", "./app"),
@@ -24,4 +24,5 @@ const service = new awsx.ecs.FargateService("nginx", {
 export const frontendURL = pulumi.interpolate`https://${listener.endpoint.hostname}/`;
 export const kibanaUrl =  pulumi.interpolate`https://${elasticStream.kibanaEndpoint}/`;
 export const elasticStreamArn = elasticStream.arn;
-export const serviceArn = service.urn;
+export const lambdaArn = logStreamLambda.arn;
+export const lambdaName = logStreamLambda.name;
--- fargate/logging/elasticsearch.ts
+++ fargate/logging/elasticsearch.ts
@@ -1,9 +1,13 @@
 import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
 
-const logStreamName = "nginx-log-stream";
+const nameNginxLogStream = "nginx-log-stream";
+const nameCloudwatchLogGroup = "nginx-log";
+const nameLogGroupSubscriptionFilter = `${nameCloudwatchLogGroup}-subscription-filter`;
+const current = aws.getCallerIdentity({});
+export const accountId = current.then(current => current.accountId);
 
-export const elasticStream = new aws.elasticsearch.Domain(logStreamName, {
+export const elasticStream = new aws.elasticsearch.Domain(nameNginxLogStream, {
     clusterConfig: {
         instanceType: "t2.micro.elasticsearch",
     },
@@ -14,27 +18,16 @@ export const elasticStream = new aws.elasticsearch.Domain(logStreamName, {
     elasticsearchVersion: "1.5",
 });
 
-const elasticStreamLambdaIamPolicy = new aws.iam.Policy(`${logStreamName}-lambda-policy`, {
-    policy: {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Action": [
-                    "es:*",
-                ],
-                "Effect": "Allow",
-                "Resource": pulumi.interpolate`${elasticStream.arn}/*`,
-            },
-        ],
-    },
+export const cloudwatchLogGroup = new aws.cloudwatch.LogGroup(nameCloudwatchLogGroup,{
+    retentionInDays: 7,
 });
 
-const elasticStreamLambdaIamRole = new aws.iam.Role(`${logStreamName}-lambda-role`, {
+const elasticStreamLambdaIamRole = new aws.iam.Role(`${nameNginxLogStream}-lambda-role`, {
     assumeRolePolicy: {
         "Version": "2012-10-17",
         "Statement": [
             {
-                Action: ['sts:AssumeRole'],
+                Action: ["sts:AssumeRole"],
                 Effect: "Allow",
                 Principal: {
                     Service: ["lambda.amazonaws.com"],
@@ -42,20 +35,47 @@ const elasticStreamLambdaIamRole = new aws.iam.Role(`${logStreamName}-lambda-rol
             },
         ],
     },
+    managedPolicyArns: [
+        "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+    ]
 });
 
-const elasticStreamLambdaIamRolePolicy = new aws.iam.RolePolicy(`${logStreamName}-lambda-role-policy`, {
+new aws.iam.RolePolicy(`${nameNginxLogStream}-lambda-role-elastic-policy`, {
     role: elasticStreamLambdaIamRole,
-    policy: elasticStreamLambdaIamPolicy.policy,
+    policy: {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": ["es:*"],
+                "Effect": "Allow",
+                "Resource": pulumi.interpolate`${elasticStream.arn}/*`,
+            },
+        ],
+    },
 });
 
-const logStreamLambda = new aws.lambda.Function(`LogsToElasticsearch-${logStreamName}`, {
+new aws.iam.RolePolicy(`${nameNginxLogStream}-lambda-role-invoke-policy`, {
+    role: elasticStreamLambdaIamRole,
+    policy: {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                Action: ["lambda:InvokeFunction"],
+                Effect: "Allow",
+                Resource: ["*"],
+            },
+        ],
+    },
+});
+
+export const logStreamLambda = new aws.lambda.Function(`LogsToElasticsearch-${nameNginxLogStream}`, {
     handler: "index.handler",
     runtime: "nodejs12.x",
     role: elasticStreamLambdaIamRole.arn,
     // relative to Pulumi.yaml
     code: new pulumi.asset.FileArchive("logging/LogsToElasticSearch"),
     description: "Ships logs from Cloudwatch to ElasticSearch",
+    timeout: 15,
     environment: {
         variables: {
             ES_ENDPOINT: elasticStream.endpoint,
@@ -63,11 +83,19 @@ const logStreamLambda = new aws.lambda.Function(`LogsToElasticsearch-${logStream
     },
 });
 
-const cloudwatchLogGroup = "nginx-log";
-export const logGroup = new aws.cloudwatch.LogGroup(cloudwatchLogGroup);
+const logStreamLambdaInvokePermission = new aws.lambda.Permission(`${nameCloudwatchLogGroup}-invoke-permission`, {
+    action: "lambda:InvokeFunction",
+    function: logStreamLambda.name,
+    // requires the region of the log service
+    principal: "logs.ap-southeast-2.amazonaws.com",
+    sourceArn: pulumi.interpolate`${cloudwatchLogGroup.arn}:*`,
+    sourceAccount: accountId,
+}, {
+    dependsOn: [logStreamLambda],
+});
 
-new aws.cloudwatch.LogSubscriptionFilter(`${cloudwatchLogGroup}-subscription-filter`, {
-    destinationArn: logStreamLambda.arn,
+new aws.cloudwatch.LogSubscriptionFilter(nameLogGroupSubscriptionFilter, {
+    destinationArn: pulumi.interpolate`${logStreamLambda.arn}`,
     filterPattern: "",
-    logGroup: logGroup
-})
+    logGroup: cloudwatchLogGroup,
+}, { dependsOn: [logStreamLambdaInvokePermission, logStreamLambda] });
