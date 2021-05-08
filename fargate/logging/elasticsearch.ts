8162c8e7c83efe5980a6eec400cfc40cd9e1fcbe
Shuqian Hon (SQ) <sq@waddle.com.au>
2021-05-09 08:11:03 +1000

    Create lambda execution policy

--- fargate/logging/elasticsearch.ts
+++ fargate/logging/elasticsearch.ts
@@ -1,8 +1,10 @@
 import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
 
 export const logGroup = new aws.cloudwatch.LogGroup("nginx-log");
+const logStreamName = "nginx-log-stream";
 
-export const elasticStream = new aws.elasticsearch.Domain("nginx-log-stream", {
+export const elasticStream = new aws.elasticsearch.Domain(logStreamName, {
     clusterConfig: {
         instanceType: "t2.micro.elasticsearch",
     },
@@ -11,4 +13,39 @@ export const elasticStream = new aws.elasticsearch.Domain("nginx-log-stream", {
         volumeSize: 10,
     },
     elasticsearchVersion: "1.5",
+});
+
+const elasticStreamLambdaIamPolicy = new aws.iam.Policy(`${logStreamName}-lambda-policy`, {
+    policy: {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": [
+                    "es:*",
+                ],
+                "Effect": "Allow",
+                "Resource": pulumi.interpolate`${elasticStream.arn}/*`,
+            },
+        ],
+    },
+});
+
+const elasticStreamLambdaIamRole = new aws.iam.Role(`${logStreamName}-lambda-role`, {
+    assumeRolePolicy: {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                Action: ['sts:AssumeRole'],
+                Effect: "Allow",
+                Principal: {
+                    Service: ["lambda.amazonaws.com"],
+                },
+            },
+        ],
+    },
+});
+
+const elasticStreamLambdaIamRolePolicy = new aws.iam.RolePolicy(`${logStreamName}-lambda-role-policy`, {
+    role: elasticStreamLambdaIamRole,
+    policy: elasticStreamLambdaIamPolicy.policy,
 });
